2e802a6e1a96c5ea75351d063e5dc4fa64a08cb7
renan lima <renangenito@hotmail.com>
2021-10-07 16:19:41 -0300

    Arquivos do projeto bolsa de valores

---
+++ app/app.ts
@@ -0,0 +1,6 @@
+import { Negociacao } from "./models/negociacao.js";
+const negociacao = new Negociacao(new Date(), 100, 100);
+console.log(negociacao);
+
+console.log(negociacao.volume);
+alert("oi")
